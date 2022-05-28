bf240cf1ba9d5fe34e8285c8de5d4e30b8c3d7a2
Denis Klintukhov <denis.klintukhov@gmail.com>
2022-05-28 12:43:59 +0500

    add: ip filter

--- test_config.cpp
+++ test_config.cpp
@@ -3,6 +3,6 @@
 #include <boost/test/included/unit_test.hpp>
 #include "config.h"
 
-BOOST_AUTO_TEST_CASE(config_test) {
+BOOST_AUTO_TEST_CASE(test_config) {
     BOOST_CHECK(PROJECT_VER == "0.0.1");
 }
