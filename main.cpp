df23475550946a6e4222d0b1dd6fba542f783a52
Denis Klintukhov <denis.klintukhov@gmail.com>
2023-09-17 20:02:50 +0300

    compiler warnings for Windows and Apple

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <stdint.h>
+#include <stdint.h>
 
 int main(int, char **)
 {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
     std::cout << "Hello, from cpp_practice!\n" << std::endl;
 }
