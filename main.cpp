7fd2812c7bf701822843e56f8cd825c3676992ab
Denis Klintukhov <denis.klintukhov@gmail.com>
2023-09-18 13:18:48 +0300

    fix: compiler options

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <stdint.h>
-#include <stdint.h>
 
 int main(int, char **)
 {
     std::ios::sync_with_stdio(0);
     std::cin.tie(0);
 
-    std::cout << "Hello, from cpp_practice!\n" << std::endl;
+    std::cout << "Hello, from cpp_practice!\n";
 }
